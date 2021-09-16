2cfc16f34a3eb0bf841acde5dc72a97a39dc6566
Rober Lin <ecbr44@gmail.com>
2021-09-16 19:51:43 +0800

    all pages

--- src/views/EditBlog.vue
+++ src/views/EditBlog.vue
@@ -1,29 +1,75 @@
 <script>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { ref } from '@vue/runtime-core';
-
+import { onMounted, ref } from '@vue/runtime-core';
+import { useEditor, EditorContent,FloatingMenu  } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import TextAlign from '@tiptap/extension-text-align';
+import TextStyle from '@tiptap/extension-text-style';
+import { Color } from '@tiptap/extension-color';
+import Image from '@tiptap/extension-image';
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Link from '@tiptap/extension-link'
+import Strike from '@tiptap/extension-strike'
 
 export default {
+    components: {
+        EditorContent,
+        FloatingMenu
+    },
     setup(){
-        const ckeditorVal = ref('')
 
-        const onEditorInput = (e)=>{
-            ckeditorVal.value = e;
-        };
+        const editor = useEditor({
+            content: '<p>I’m running tiptap with Vue.js. 🎉</p>',
+            extensions: [
+                StarterKit,
+                TextStyle,
+                Color,
+                Image,
+                Bold,
+                Link,
+                Italic,
+                Strike,
+                TextAlign.configure({
+                    types: ['heading', 'paragraph'],
+                }),
+            ],
+        })
 
         const handCkData = ()=>{
-            console.log(ckeditorVal.value);
+            // console.log(editor.value.options.content);
+            console.log(editor.value.getHTML());
         };
+
+        const addImage = ()=>{
+            const url = window.prompt('URL')
+
+            if (url) {
+                editor.value.chain().focus().setImage({ src: url }).run()
+            }
+        };
+
+        const setLink = ()=>{
+            const url = window.prompt('URL')
+
+            editor.value
+                .chain()
+                .focus()
+                .extendMarkRange('link')
+                .setLink({ href: url })
+                .run()
+            
+        };
+
+        onMounted(()=>{
+            console.log(editor);
+        })
+        
 
         return {
             handCkData,
-            onEditorInput,
-            ckeditorVal,
-            editor: ClassicEditor,
-            editorData: '',
-            editorConfig: {
-                height:'300px',
-            },
+            editor,
+            addImage,
+            setLink
         }
     }
 }
@@ -69,7 +115,42 @@ export default {
           </tr>
           <tr>
               <td>
-                  <ckeditor @input="onEditorInput" :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+                  <input type="file" class="fileUpload" name="" id="">
+                  <button>上傳圖片</button>
+              </td>
+          </tr>
+          <tr>
+              <td>
+                  <div v-if="editor">
+                        <button @click="addImage">新增圖片</button>
+                        <button @click="editor.chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+      bold
+    </button>
+    <button @click="editor.chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+      italic
+    </button>
+    <button @click="setLink" :class="{ 'is-active': editor.isActive('link') }">
+      link
+    </button>
+    <button @click="editor.chain().focus().unsetLink().run()" v-if="editor.isActive('link')">
+      remove
+    </button>
+    <button @click="editor.chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+      strike
+    </button>
+                        <input type="color" @input="editor.chain().focus().setColor($event.target.value).run()" :value="editor.getAttributes('textStyle').color" />
+                        <button @click="editor.chain().focus().setTextAlign('left').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }">left</button>
+                        <button @click="editor.chain().focus().setTextAlign('center').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }">center</button>
+                        <button @click="editor.chain().focus().setTextAlign('right').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }">right</button>
+                        <button @click="editor.chain().focus().setTextAlign('justify').run()" :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }">justify</button>
+                        <button @click="editor.chain().focus().unsetTextAlign().run()">set default</button>
+                        <floating-menu :editor="editor" v-if="editor">
+                            <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">h1</button>
+                            <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()" :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">h2</button>
+                            <button @click="editor.chain().focus().toggleBulletList().run()" :class="{ 'is-active': editor.isActive('bulletList') }">bullet list</button>
+                        </floating-menu>
+                        <editor-content :editor="editor" />
+                    </div>
               </td>
           </tr>
           <tr>
@@ -81,10 +162,23 @@ export default {
       </table>
   </div>
 </template>
-
 <style scoped lang="scss">
 @import '~@/assets/scss/_mixins.scss';
-
+/deep/ .ProseMirror{
+    background-color: #fff;
+    padding: 10px;
+    height: 300px;
+    overflow:auto;
+    img{
+        max-width: $full;
+    }
+    a{
+        text-decoration: underline;
+    }
+}
+.fileUpload{
+    color: #fff;
+}
 .edit{
     max-width: 800px;
     width:$full;
